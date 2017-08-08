9131e303559a6f2aca4a6e8cbaf4ecc625cc7144
bigGreenPeople <1243596620@qq.com>
2017-08-08 11:21:03 +0800

    Stack Context域与ActionContext的关系

--- struts2_04/src/com/fj/action/InterceptorAction.java
+++ struts2_04/src/com/fj/action/InterceptorAction.java
@@ -1,5 +1,6 @@
 package com.fj.action;
 
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class InterceptorAction extends ActionSupport {
@@ -13,6 +14,14 @@ public class InterceptorAction extends ActionSupport {
 	public String add() throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("add执行");
+		ActionContext context = ActionContext.getContext();
+		//向context中放入值
+		context.put("name", "付杰");
+		//从context取值
+		System.out.println(context.get("name"));
+		//在s:debug中我们看的name出现在了Stack Context中
+		//说明put就是向Stack Context放数据Stack Context就是ActionContext
+		
 		return "ok";
 	}
 }
