4ab65c1b34920cfb47ef65ab29a5e316eb12d00e
bigGreenPeople <1243596620@qq.com>
2017-08-09 17:01:56 +0800

    Hibernate基本配置

--- hibernate_01/src/com/fj/demo/Demo.java
+++ hibernate_01/src/com/fj/demo/Demo.java
@@ -7,15 +7,13 @@ import org.hibernate.cfg.Configuration;
 import org.junit.Test;
 
 import com.fj.domain.User;
+import com.fj.utils.HibernateUtils;
 
 public class Demo {
 	
 	@Test
 	public void testAdd(){
-		//加载配置信息
-		Configuration configuration = new Configuration().configure();
-		//创建工厂	到这一步的时候就会在数据库里面创建表
-		SessionFactory sessionFactory = configuration.buildSessionFactory();
+		SessionFactory sessionFactory = HibernateUtils.getSessionFactory();
 		//得到session
 		Session session = sessionFactory.openSession();
 		//得到事务
