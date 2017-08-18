60caf03be823c38aa62b906d6be7440e507e97b6
bigGreenPeople <1243596620@qq.com>
2017-08-18 10:03:47 +0800

    spring整合struts2

---
+++ spring_struts/src/com/fj/action/StrutsAction.java
@@ -0,0 +1,12 @@
+package com.fj.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class StrutsAction extends ActionSupport {
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("action...............");
+		return NONE;
+	}
+}
